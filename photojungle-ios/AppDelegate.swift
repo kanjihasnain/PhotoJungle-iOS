5f9ee1cbd348e08ca61a06d26b1aba64a1a6abc2
HasnainKanji <kanji.hasnain@gmail.com>
2021-07-01 13:18:16 +0500

    Initial Commit

---
+++ photojungle-ios/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  photojungle-ios
+//
+//  Created by Kanji on 01/07/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+}
